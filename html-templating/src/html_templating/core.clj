10a23d7ec6f81b4b1ce91816c34ac71bdf91c236
Jon <haygoodjon@gmail.com>
2017-10-01 18:51:15 -0400

    templatin

--- html-templating/src/html_templating/core.clj
+++ html-templating/src/html_templating/core.clj
@@ -1,7 +1,28 @@
 (ns html-templating.core
- (:require [selmer.parser :as selmer]))
+ (:require [selmer.parser :as selmer]
+           [selmer.filters :as filters]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(filters/add-filter! :empty? empty?)
+
+(filters/add-filter! :foo
+                     (fn [x] [:safe (.toUpperCase x)]))
+
+(println (selmer/render "{% if files|empty? %}no files{% else %}files{% endif %}" {:files []}))
+(println (selmer/render "{{x}}" {:x "<div>hello, am I safe?</div>"}))
+
+(selmer/add-tag!
+  :image
+  (fn [args context-map]
+      (str "<img src=" (first args) "/>")))
+
+(println (selmer/render "{% image \"html://foo.com/logo.jpg\" %}" {}))
+
+(selmer/add-tag!
+  :uppercase
+  (fn [args context-map content]
+      (.toUpperCase (get-in content [:uppercase :content])))
+  :enduppercase)
+
+(println (selmer/render "{% uppercase %}foo {{bar}} baz {% enduppercase %}" {:bar "inject"}))
+
+(println "alright")
